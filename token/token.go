1caaa1f1e0a6eb38ceb8777d327a65593b3c8af2
Norifumi Kimura <norical496@gmail.com>
2022-02-09 21:33:53 +0900

    Added bang, slash, minus, asterisk, lt, gt

--- token/token.go
+++ token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"
 
 	//Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	SLASH    = "/"
+	ASTERISK = "*"
+
+	LT = "<"
+	GT = ">"
 
 	//Delimiters
 	COMMA     = ","
