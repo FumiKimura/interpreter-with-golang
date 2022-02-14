cb85e4043d70a16a7961e7583e270c950cf73614
Norifumi Kimura <norical496@gmail.com>
2022-02-14 20:46:57 +0900

    Added curTokenIs and peekTokenIs

--- parser/parser.go
+++ parser/parser.go
@@ -49,3 +49,35 @@ func (p *Parser) parseStatement() ast.Statement {
 		return nil
 	}
 }
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) expectPeek() bool {
+
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
