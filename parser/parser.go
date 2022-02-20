19bd0cbafc02a3ecf902de523b4f74ed6d9ff8cb
Norifumi Kimura <norical496@gmail.com>
2022-02-20 16:01:08 +0900

    Added test for parsing expression

--- parser/parser.go
+++ parser/parser.go
@@ -15,7 +15,15 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l,
@@ -114,4 +122,12 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 		p.nextToken()
 	}
 	return stmt
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn){
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn){
+	p.infixParseFns[tokenType] = fn
 }
