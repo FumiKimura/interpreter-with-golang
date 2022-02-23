10f52d1eb52f49c43e036a97c79559d60942b16b
Norifumi Kimura <norical496@gmail.com>
2022-02-23 21:47:44 +0900

    Added noPrefixParseFnError

--- parser/parser.go
+++ parser/parser.go
@@ -165,6 +165,7 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
@@ -188,4 +189,9 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 	lit.Value = value
 
 	return lit
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
 }
