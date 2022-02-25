1bfeeac3855268649b249a979b47c5207b055c70
Norifumi Kimura <norical496@gmail.com>
2022-02-25 23:22:52 +0900

    Changed parse expression

--- parser/parser.go
+++ parser/parser.go
@@ -192,6 +192,18 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 		return nil
 	}
 	leftExp := prefix()
+
+	for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+		infix := p.infixParseFns[p.peekToken.Type]
+		if infix == nil {
+			return leftExp
+		}
+
+		p.nextToken()
+
+		leftExp = infix(leftExp)
+	}
+
 	return leftExp
 }
 
