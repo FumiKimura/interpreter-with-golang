6e458dae248152540d4370c24091448aba7bcafb
Norifumi Kimura <norical496@gmail.com>
2022-04-12 07:25:44 +0900

    Added Boolean object

--- object/object.go
+++ object/object.go
@@ -6,6 +6,7 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
 )
 
 type Object interface {
@@ -17,5 +18,12 @@ type Integer struct {
 	Value int64
 }
 
+type Boolean struct {
+	Value bool
+}
+
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
