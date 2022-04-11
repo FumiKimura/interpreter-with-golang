8160072a71b86fa173ec79613bb53c26d48647b7
Norifumi Kimura <norical496@gmail.com>
2022-04-12 07:28:01 +0900

    Added Null Object

--- object/object.go
+++ object/object.go
@@ -7,6 +7,7 @@ type ObjectType string
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 type Object interface {
@@ -22,8 +23,13 @@ type Boolean struct {
 	Value bool
 }
 
+type Null struct{}
+
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+func (n *Null) Inspect() string  { return NULL_OBJ }
+func (n *Null) Type() ObjectType { return NULL_OBJ }
