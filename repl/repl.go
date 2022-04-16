d164e522044088b99cd77022b7bf5d91c92575ee
Norifumi Kimura <norical496@gmail.com>
2022-04-16 22:50:50 +0900

    Added map environment for identifiers

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/FumiKimura/interpreter-with-golang/evaluator"
 	"github.com/FumiKimura/interpreter-with-golang/lexer"
+	"github.com/FumiKimura/interpreter-with-golang/object"
 	"github.com/FumiKimura/interpreter-with-golang/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
